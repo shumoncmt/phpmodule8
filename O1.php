f3387d1ac487d1eeab98a65a8be5303121ea299e
shumoncmt <shomoncmt@gmail.com>
2024-10-29 12:40:19 +0600

    aa
    aa

--- O1.php
+++ O1.php
@@ -1,15 +1,22 @@
 <?php
-// class fruit{
-//     //code goes here Function call class
-// }
 
-class Fruit{
+class parson{
     public $name;
-    public $color;
-    function set_name($name){
-        $this->name = $name;
-    }
-    function get_name(){
-        return $this->name;
+    public $age;
+    function  introduce(){
+        echo "Hi, I am {$this->name}, and I am {$this->age} years Old \n" ;
+
     }
 }
+$joni = new parson(); //Object
+$joni->name = "shakil";
+$joni->age = 25;
+$joni->introduce();
+// echo "Hi, I am {$joni->name}, and I am {$joni->age} years Old \n" ;
+
+
+$jow = new parson();
+$jow->name= "Abir";
+$jow->age = 36;
+$jow->introduce();
+// echo "Hi, I am {$jow->name}, I am {$joni->age} years Old";
