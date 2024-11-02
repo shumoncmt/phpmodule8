210300a26dfe02f47492ce6cbc7a2abc56a9a156
shumoncmt <shomoncmt@gmail.com>
2024-11-02 13:28:16 +0600

    aff
    add

---
+++ rabbil/part6.php
@@ -0,0 +1,46 @@
+<?php
+// class Father {
+
+//     public $msg="Message ABC";
+
+//     final public function print100() {
+//         for($i=0;$i<=100;$i++){
+//          echo "$i <br/>";
+//         }
+//      }
+// }
+
+// class Son extends Father{
+   
+
+//     //override
+//     public function print100() {
+//         for($i=0;$i<=10;$i++){
+//          echo "$i <br/>";
+//         }
+//      }
+
+// }
+
+
+// $objFather= new Father();
+// $objSon= new Son();
+
+
+// $objFather->print100();
+// $objSon->print100();
+
+class BaseClass{
+    public function test(){
+        echo "BaseClass :: Test () called \n";
+    }
+  final public function moreTesting(){
+        echo "BaseClass :: MoreTesting() called \n";
+    }
+}
+
+class childClass extends BaseClass{
+    public function moreTesting(){
+        echo "ChildClass::moreTesting() called\n";
+     }
+}
